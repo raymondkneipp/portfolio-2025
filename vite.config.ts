5fab004afb99956a2f94f3c13b962e1ffd1d721b
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2025-11-02 21:55:32 -0500

    add nitro for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite"
 
 const config = defineConfig(async ({ mode }) => {
   process.env = {
@@ -21,6 +22,7 @@ const config = defineConfig(async ({ mode }) => {
       }),
       tailwindcss(),
       tanstackStart(),
+      nitro()
       viteReact(),
     ],
   };
